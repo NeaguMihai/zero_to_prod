6504c6d36b5bd0b6bb0b1378d41f93a68d5586b5
Mihai <mihain199961@gmail.com>
2023-01-29 15:47:41 +0000

    Added first test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero_to_prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind random port");
+    run(listener)?.await
 }
