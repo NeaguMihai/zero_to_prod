975a55c5817bd6298bb7cb3096cf3b17c540bcc8
Mihai <mihain199961@gmail.com>
2023-01-29 15:50:19 +0000

    chore: format code and fixes
    - add circleci config
    
    - updateconfig
    
    - updated orb
    
    - update cfg
    
    - updated cargo.toml
    
    - clippy fix
    
    - add circleci config
    
    - updated cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use actix_web::{web, App, HttpResponse, HttpServer, dev::Server};
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
