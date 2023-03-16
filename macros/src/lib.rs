fcd439f8d59a11a2593b5335a23b2b517859041f
Mihai <mihain199961@gmail.com>
2023-03-16 02:49:03 +0200

    feat(axum-routing-macro): Complete controller derive
    Finished the controller derive macro.
    
    Refactored the macro function by breaking it into smaller functions.

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -51,7 +51,7 @@ pub fn controller(attr: TokenStream, item: TokenStream) -> TokenStream {
         let path = path
             .unwrap()
             .unwrap()
-            .split(":")
+            .split(':')
             .collect::<Vec<&str>>()
             .get(1)
             .unwrap()
@@ -82,19 +82,58 @@ pub fn controller_derive(input: TokenStream) -> TokenStream {
     let base_route = parse_controller_attributes(attrs.clone(), 0);
     let file_path = parse_controller_attributes(attrs, 1);
 
-    println!("base_route: {:?}", base_route);
-    println!("base_route: {:?}", file_path);
-    // let file_content = read_to_string(file_path.clone()).expect("Failed to open routes file");
-    // let start_time = Instant::now();
-    // let syn_file = parse_file(&file_content).expect("Failed to parse file");
-    // let end = Instant::now();
-    // println!("Time to parse file: {:?}", end - start_time);
-
     let file = File::open(file_path).expect("Failed to open routes file");
     let start_time = Instant::now();
     let reader = BufReader::new(file);
+    let lines = reader.lines();
+    let routes = find_routes(lines);
+    let end = Instant::now();
+    println!("Time to read file: {:?}", end - start_time);
+
+    let routes_tokens = convert_routes_to_token_stream(routes);
+
+    let name = input.ident;
+    let generated_code = quote! {
+        use app_core::traits::Controller;
+        impl Controller for #name {
+            fn name(&self) -> &'static str {
+                stringify!(#name)
+            }
+            fn base_path(&self) -> &'static str {
+                #base_route
+            }
+            fn register_routes(&self, router: Router) -> Router {
+                router
+                #( #routes_tokens )*
+
+            }
+        }
+
+    };
+    TokenStream::from(generated_code)
+}
+
+fn convert_routes_to_token_stream(
+    routes: Vec<(String, String, String)>,
+) -> Vec<proc_macro2::TokenStream> {
+    let axum_ident = Ident::new("axum", proc_macro2::Span::call_site());
+    let route_ident = Ident::new("routing", proc_macro2::Span::call_site());
+    let routes_tokens = routes
+        .iter()
+        .map(|(method_name, route, function_name)| {
+            let method_name = Ident::new((method_name).as_str(), proc_macro2::Span::call_site());
+            let route = LitStr::new(route, proc_macro2::Span::call_site());
+            let function_name = Ident::new(function_name, proc_macro2::Span::call_site());
+            quote! {
+                .route(#route, #axum_ident::#route_ident::#method_name(#function_name))
+            }
+        })
+        .collect::<Vec<_>>();
+    routes_tokens
+}
+
+fn find_routes(mut lines: std::io::Lines<BufReader<File>>) -> Vec<(String, String, String)> {
     let mut routes: Vec<(String, String, String)> = Vec::new();
-    let mut lines = reader.lines();
     loop {
         let line = lines.next();
         if line.is_none() {
@@ -130,42 +169,7 @@ pub fn controller_derive(input: TokenStream) -> TokenStream {
             ));
         }
     }
-    let end = Instant::now();
-    println!("Time to read file: {:?}", end - start_time);
-    let routes_tokens = routes
-        .iter()
-        .map(|(method_name, route, function_name)| {
-            let method_name = Ident::new(
-                (method_name).as_str(),
-                proc_macro2::Span::call_site(),
-            );
-            let route = LitStr::new(route, proc_macro2::Span::call_site());
-            let function_name = Ident::new(function_name, proc_macro2::Span::call_site());
-            quote! {
-                .route(#route, #method_name(#function_name))
-            }
-        })
-        .collect::<Vec<_>>();
-    let name = input.ident;
-    let generated_code = quote! {
-        use app_core::traits::Controller;
-        impl Controller for #name {
-            fn name(&self) -> &'static str {
-                stringify!(#name)
-            }
-            fn base_path(&self) -> &'static str {
-                "test"
-            }
-            fn register_routes(&self, router: Router) -> () {
-                router
-                #( #routes_tokens )*;
-                ()
-            }
-        }
-
-    };
-    println!("generated_code: {:?}", generated_code.to_string());
-    TokenStream::from(generated_code)
+    routes
 }
 
 fn parse_controller_attributes(attrs: Vec<Attribute>, index: u16) -> String {
@@ -185,32 +189,10 @@ fn parse_controller_attributes(attrs: Vec<Attribute>, index: u16) -> String {
     base_route
 }
 
-//TODO for future me: gather all controllers in the same folter
-// After that, generate a file with all the routes. I think I should run a macro that activates at runtime
-// This macro should walk all files in the controller directory and generate a file with all the routes
-// To know the routes I should have the route macro generate struct const for each route
-// The struct must look something like:
-// struct Route {
-//     path: &'static str,
-//     method: &'static str,
-//     handler: fn() -> (), //here I can use the ident of the function
-// etcasfaasdasdafsasdasdasdasdssa 5
-// }
-
-fn route(attr: TokenStream, item: TokenStream, method_type: &str) -> TokenStream {
+fn route(_attr: TokenStream, item: TokenStream, _method_type: &str) -> TokenStream {
     let stream_copy = item.to_string().parse().unwrap();
 
     let input = parse2::<ItemFn>(stream_copy).unwrap();
-    // let ident = input.sig.ident.clone().to_string();
-    // let route = parse2::<LitStr>(attr.into()).unwrap().value();
-
-    // let route_name = Ident::new(
-    //     (input.sig.ident.to_string() + "_route")
-    //         .to_uppercase()
-    //         .as_str(),
-    //     proc_macro2::Span::call_site(),
-    // );
-    // let method_type = method_type.to_string();
 
     let expanded = quote! {
         #input
